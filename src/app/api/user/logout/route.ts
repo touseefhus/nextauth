63b92a5de92ad2625c856dec5d98fb1cfefc8cc1
root <root@DESKTOP-9SDJB3D.>
2024-11-21 17:02:15 +0500

    push logout route

---
+++ src/app/api/user/logout/route.ts
@@ -0,0 +1,27 @@
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/model/user.models";
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs"
+import { sendEmail } from "@/helpers/mailer";
+import jwt from "jsonwebtoken"
+connect()
+
+export async function GET(request: NextRequest) {
+    try {
+        const response = NextResponse.json(
+            {
+                message: "User logout successfully",
+                succeess: true,
+            }
+        )
+        response.cookies.set("token", "", {
+            httpOnly: true,
+            expires: new Date(0),
+        });
+
+
+
+    } catch (error: any) {
+        return NextResponse.json({ error: error.message }, { status: 500 })
+    }
+}
